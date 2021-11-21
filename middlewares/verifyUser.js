495f7ddd9ff91c4b0cc5f983c5011f5d904cd5b1
rajatgautam755421 <rg755421@gmail.com>
2021-11-21 09:41:11 +0545

    fifth commit

--- middlewares/verifyUser.js
+++ middlewares/verifyUser.js
@@ -3,7 +3,10 @@ const jwt = require("jsonwebtoken");
 //Checking For User Authentication
 function verify(req, res, next) {
   const token = req.header("auth-token");
-  if (!token) return res.status(403).send("Not Authenticated");
+  if (!token) return res.status(403).json({
+      status : "fail",
+      message : "Not Authenticated"
+  });
   try {
     jwt.verify(token, process.env.TOKEN_SECRET);
     next();
