12f2249b19ba714e927a1a9fe1883ea6abba4c05
rajatgautam755421 <rg755421@gmail.com>
2021-11-23 22:15:07 +0545

    8th commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { connect } = require("mongoose");
 
 connect(
-  "mongodb+srv://rajat:rajat12345@cluster0.xtjbz.mongodb.net/UsersModel",
+  "mongodb+srv://rajat:12345@cluster0.uaz2j.mongodb.net/UsersModel?retryWrites=true&w=majority",
   (error) => {
     if (error) {
       console.log(error.message);
